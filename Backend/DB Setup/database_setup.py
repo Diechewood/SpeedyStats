723781b50ce4ad5289f4c95e963ed107006f6436
Diechewood <elian721@gmail.com>
2023-11-16 14:51:15 -0600

    Add logging to database_setup.py

--- Backend/DB Setup/database_setup.py
+++ Backend/DB Setup/database_setup.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from models import Base
 from database_config import DATABASE_URI
+import logging
+from sqlalchemy.exc import SQLAlchemyError
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def setup_database():
     engine = create_engine(DATABASE_URI)
