6168cb85e5b68b1f84224aa7c0652a2de969cb34
Diechewood <elian721@gmail.com>
2024-01-28 21:33:27 -0600

    SP SpeedDataManagerService.py

--- Backend/Services/SpeedDataManagerService.py
+++ Backend/Services/SpeedDataManagerService.py
@@ -1,1 +1,25 @@
+# SpeedDataManagerService.py
+from pymongo import MongoClient
+from DatabaseConfig import MongoDBConfig
 
+class SpeedDataManagerService:
+    def __init__(self):
+        self.client = MongoClient(f"mongodb://{MongoDBConfig.USER}:{MongoDBConfig.PASSWORD}@{MongoDBConfig.HOST}:{MongoDBConfig.PORT}")
+        self.db = self.client[MongoDBConfig.DATABASE]
+        self.collection = self.db['raw_speed_data']
+
+    def record_speed_data(self, user_id, speed, timestamp, location):
+        try:
+            self.collection.insert_one({
+                "user_id": user_id,
+                "speed": speed,
+                "timestamp": timestamp,
+                "location": location
+            })
+            return True
+        except Exception as e:
+            print(f"Error recording speed data: {e}")
+            return False
+
+    def __del__(self):
+        self.client.close()
