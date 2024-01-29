d13e87c542fbb06af349e784ab7971b11c7aba2a
Diechewood <elian721@gmail.com>
2024-01-28 21:32:35 -0600

    Starting point DatabaseConfig.py

--- Backend/Database/DatabaseConfig.py
+++ Backend/Database/DatabaseConfig.py
@@ -1,1 +1,16 @@
+# DatabaseConfig.py
+
+class PostgreSQLConfig:
+    HOST = 'localhost'
+    PORT = '5432'
+    DATABASE = 'longboard_speed_tracking'
+    USER = 'postgres_user'
+    PASSWORD = 'postgres_password'
+
+class MongoDBConfig:
+    HOST = 'localhost'
+    PORT = '27017'
+    DATABASE = 'longboard_speed_data'
+    USER = 'mongodb_user'
+    PASSWORD = 'mongodb_password'
 
